f0417a2470e2b5381e070671a4002b44bf558a59
Parag0712 <paragvadgama123@gmail.com>
2024-03-05 14:13:29 +0530

    user reducer added

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { userReducer } from "./reducers/userReducers";
+import { userAPI } from "./api/userAPI";
 
 export const server = import.meta.env.VITE_SERVER_BASE_URL;
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        [userAPI.reducerPath]:userAPI.reducer,
+        [userReducer.name]:userReducer.reducer
+    },
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch 
